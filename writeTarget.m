0786463d0d632acff71aa77fcd0ed54d2a341186
Yang Hong <yangster@stanford.edu>
2012-04-22 21:30:45 -0700

    forgot to add modified writeTarget function

--- writeTarget.m
+++ writeTarget.m
@@ -1,4 +1,7 @@
-fid = fopen('Target_1.csv','w');
+function writeTarget(filename)
+% writes predicted DIH as .csv to target filename (i.e. 'Target_1.csv')
+
+fid = fopen(filename,'w');
 fprintf(fid,'MemberID,ClaimsTruncated,DaysInHospital\n');
 fprintf(fid,'%d,%d,%f\n',[target_memberids_orig_order';target_claimsTrunc_orig_order';target_DIH']);
 fclose(fid);
