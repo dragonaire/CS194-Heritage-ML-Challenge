52fa712b23b0070ad2785882beed6f911f990c41
Yang Hong <yangster@stanford.edu>
2012-04-26 20:46:00 -0700

    working DIH only predictor

--- computeTargetDIH_DIHonly.m
+++ computeTargetDIH_DIHonly.m
@@ -1,23 +1,23 @@
-function target_DIH = computeTargetDIH_DIHonly(target, logDIH, bothyears)
+function target_DIH = computeTargetDIH_DIHonly(target, logDIH, members, bothyears)
 % find optimal # of days give prev DIH (Y2, Y3)
 constants;
 if (bothyears) 
-total = length(logDIH.comb23);  
-logDIH23 = logDIH.comb23;
-cvx_begin
-    variables logDIH_opt(total, 1);
-    minimize(norm(logDIH_opt - logDIH23));
-cvx_end
+    total = length(logDIH.comb23);  
+    logDIH23 = logDIH.comb23;
+    cvx_begin
+        variables logDIH_opt(total, 1);
+        minimize(norm(logDIH_opt - logDIH23));
+    cvx_end
 else
-total = length(logDIH.yr3);  
-cvx_begin
-    variables logDIH_opt(total, 1);
-    minimize(norm(logDIH_opt - logDIH.yr3));
-cvx_end
+    total = length(logDIH.yr3);  
+    cvx_begin
+        variables logDIH_opt(total, 1);
+        minimize(norm(logDIH_opt - logDIH.yr3));
+    cvx_end
 end
 
 DIH_opt = exp(logDIH_opt) - 1;
 
-[target_members, matched_memberids_i, all_memberids_i] = intersect(target.memberids, logDIH.members23);
+[target_members, matched_memberids_i, all_memberids_i] = intersect(target.memberids, members.comb23);
 target_DIH = 0.2.*ones(NUM_TARGETS,1);
 target_DIH(matched_memberids_i) = DIH_opt(all_memberids_i);
