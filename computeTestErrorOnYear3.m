682e34d1c8ad5bbf75b85662ed0f313017bb8b74
Andrew Chou <andrewkchou@gmail.com>
2012-05-03 23:33:03 -0700

    Added testing on the median predictor.

--- computeTestErrorOnYear3.m
+++ computeTestErrorOnYear3.m
@@ -6,38 +6,50 @@ fake_target.claimsTrunc = claimsTrunc.yr3;
 fake_target.genders = genders.yr3;
 fake_target.ages = ages.yr3;
 
+all_yr3_pred = [];
+
 yr3_pred = computeTargetDIH_sexonly(fake_target,logDIH.genders.yr2);
 disp(sprintf('TEST ERROR %f',sqrt(mean((log(DIH.yr3+1)-log(yr3_pred+1)).^2))))
+all_yr3_pred = [all_yr3_pred, yr3_pred];
 
 yr3_pred = computeTargetDIH_ageonly(fake_target,bins.yr2);
 disp(sprintf('TEST ERROR %f',sqrt(mean((log(DIH.yr3+1)-log(yr3_pred+1)).^2))))
+all_yr3_pred = [all_yr3_pred, yr3_pred];
 
 %TODO there's something wrong with the predictions. 1 prediction is missing
 %yr3_pred = computeTargetDIH_DIHonly(fake_target,logDIH,members,true);
 %disp(sprintf('TEST ERROR %f',sqrt(mean((log(DIH.yr3+1)-log(yr3_pred+1)).^2))))
+%all_yr3_pred = [all_yr3_pred, yr3_pred];
 
 yr3_pred = computeTargetDIH_agesex(fake_target,ages.yr2,genders.yr2,logDIH.yr2);
 disp(sprintf('TEST ERROR %f',sqrt(mean((log(DIH.yr3+1)-log(yr3_pred+1)).^2))))
+all_yr3_pred = [all_yr3_pred, yr3_pred];
 
 yr3_pred = computeTargetDIH_agesexdrug(fake_target,ages.yr2,genders.yr2,...
     logDIH.yr2,drugs.features2_1yr,drugs.features3_1yr);
 disp(sprintf('TEST ERROR %f',sqrt(mean((log(DIH.yr3+1)-log(yr3_pred+1)).^2))))
+all_yr3_pred = [all_yr3_pred, yr3_pred];
 
 yr3_pred = computeTargetDIH_agesexdruglab(ages.yr2,genders.yr2,logDIH.yr2,...
     fake_target.ages,fake_target.genders,drugs.features2_1yr,drugs.features3_1yr,...
     lab.features2_1yr,lab.features3_1yr);
 disp(sprintf('TEST ERROR %f',sqrt(mean((log(DIH.yr3+1)-log(yr3_pred+1)).^2))))
+all_yr3_pred = [all_yr3_pred, yr3_pred];
 
 yr3_pred = computeTargetDIH_agesexdruglab_sqrt(ages.yr2,genders.yr2,logDIH.yr2,...
     fake_target.ages,fake_target.genders,drugs.features2_1yr,drugs.features3_1yr,...
     lab.features2_1yr,lab.features3_1yr);
 disp(sprintf('TEST ERROR %f',sqrt(mean((log(DIH.yr3+1)-log(yr3_pred+1)).^2))))
+all_yr3_pred = [all_yr3_pred, yr3_pred];
 
 yr3_pred = computeTargetDIH_agesexcombo_druglab(ages.yr2,genders.yr2,logDIH.yr2,...
     fake_target.ages,fake_target.genders,drugs.features2_1yr,drugs.features3_1yr,...
     lab.features2_1yr,lab.features3_1yr);
 disp(sprintf('TEST ERROR %f',sqrt(mean((log(DIH.yr3+1)-log(yr3_pred+1)).^2))))
+all_yr3_pred = [all_yr3_pred, yr3_pred];
 
 %get median DIH for each member
-yr3_pred = median(allDIH,2);
+yr3_pred = median(all_yr3_pred,2);
+disp('MEDIAN PREDICTOR');
+disp(sprintf('TEST ERROR %f',sqrt(mean((log(DIH.yr3+1)-log(yr3_pred+1)).^2))))
 toc
